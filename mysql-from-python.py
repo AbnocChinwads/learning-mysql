159c9e6f45ca6d884aa69ad3fd875e2c2948ab53
AbnocChinwads <rnemo@hotmail.co.uk>
2020-05-22 18:09:11 +0000

    Created SQL queries within a python file

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from workspace
@@ -7,17 +8,21 @@ import pymysql
 username = os.getenv("AbnocChinwads")
 
 # Connect to the database
-connection = pymysql.connect(host= "localhost",
-                            user=username,
-                            password="",
-                            db="Chinook")
+connection = pymysql.connect(host="localhost",
+                             user=username,
+                             password="",
+                             db="Chinook")
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "select * from Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Jim', 'Bob', 'Fred']
+        # Prepare a string with the same number of
+        # placeholders as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("Delete from Friends where name in ({});".format(format_strings),
+                       list_of_names)
+        connection.commit()
+
 finally:
     # CLose the connection, whether or not the above was successful
     connection.close()
